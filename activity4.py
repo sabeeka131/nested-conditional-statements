86eb247738d1ac34f846ee7c30a5804d608e90e5
Aliza <59551300+sabeeka131@users.noreply.github.com>
2024-12-21 18:59:11 +0500

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age: "))
+if age >= 10==20:
+    print("Your age is between 10 and 20 years.")
+if age>= 20:
+      print("Your age is greater 20 years.")
+else:
+     print("Your age is less than 10 years")
